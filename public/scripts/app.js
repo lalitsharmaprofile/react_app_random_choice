8c9283b29b00188d9382564711f57a34b9ff10c0
Lalit Sharma <lalitkumar2k10>
2021-10-31 11:20:36 +0530

    usage of conditional and logical operators

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,3 @@
 "use strict";
 
-console.log("hello2");
-
-var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App"
-);
-var appRoot = document.getElementById('app');
-
-ReactDOM.render(template, appRoot);
+console.log("es6-let-const");
--- src/app.js
+++ src/app.js
@@ -1,6 +1,41 @@
 console.log("hello2");
 
-var template = <h1>Indecision App</h1>;
+var app = {
+	title: 'Random choice app',
+	subtitle: 'Helps making better choices!',
+	options: [
+		'one item',
+		'two item'
+		]
+}
+
+function getItems(options){
+	if (options.length){
+		return "here are the options:"
+	}
+	else{
+		return "no options"
+	}
+}
+
+// <p>{getItems(app.options)}</p>
+
+var template = (
+	<div>
+		<h1>{app.title}</h1>
+		{app.subtitle && <p>{app.subtitle}</p>}
+		<p>{app.options && app.options.length?"here are the options:":"no options"}</p>
+	</div>
+	);
+
+var templateTwo = (
+	<div>
+		<h1>Lalit Sharma</h1>
+		<p>Age: 26</p>
+		<p>Location: New Delhi</p>
+	</div>
+	);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
